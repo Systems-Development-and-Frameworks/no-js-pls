43bbfbf8d13ca3f2ff26b07c0adee94c99069abb
Stefan Manthey <Stefan.Manthey@student.htw-berlin.de>
2020-10-14 19:37:06 +0200

    Add vue.js webapp for exercise-1 with basic news voting functionality

---
+++ webapp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <NewsList></NewsList>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import NewsList from '@/components/NewsList.vue';
+
+@Component({
+  components: {
+    NewsList,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
