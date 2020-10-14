e993f1c60618d5f1bc2c65aac3aed4e0c0f29c42
Stefan Manthey <Stefan.Manthey@student.htw-berlin.de>
2020-10-14 19:37:06 +0200

    Add vue.js webapp for exercise-1 with basic news voting functionality

---
+++ webapp/src/components/NewsForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit="submitTitle" v-on:submit.prevent="onsubmit">
+    <input type="text" v-model="title" placeholder="Type title here">
+    <button type="submit">Create</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Emit } from 'vue-property-decorator';
+
+@Component
+export default class NewsForm extends Vue {
+  public title: string = '';
+
+  @Emit('sendTitle')
+  public submitTitle(): string {
+    const newTitle = this.title;
+    this.title = '';
+    return newTitle;
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
