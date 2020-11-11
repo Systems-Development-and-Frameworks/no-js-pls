8a237e52fa6604ba155a47092eddc8c2a609c5d1
Stefan Manthey <Stefan.Manthey@student.htw-berlin.de>
2020-11-11 16:43:14 +0100

    Fixed error in main.ts

--- webapp/src/components/NewsForm/NewsForm.stories.ts
+++ webapp/src/components/NewsForm/NewsForm.stories.ts
@@ -8,18 +8,6 @@ export default {
       action: 'Create Item',
     },
   },
-  /* parameters: {
-    a11y: {
-      // optional selector which element to inspect
-      element: '#root',
-      // axe-core configurationOptions (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#parameters-1)
-      config: {},
-      // axe-core optionsParameter (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter)
-      options: {},
-      // optional flag to prevent the automatic check
-      manual: true,
-    },
-  }, */
 };
 
 // @ts-ignore
